4c84eaebacccbf658c60e4d2d10420e9cc74a3a3
WinnieCWu <winniechn7@gmail.com>
2022-02-21 10:50:48 -0800

    generated HTML file from terminal

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const profileDataArgs = process.argv.slice(2)
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//replace with one-liner using assignment destructuring
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
-    //This..
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==========');
+// const printProfileData = profileDataArr => {
+//     //This..
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('==========');
 
-    //is the same as this..
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+//     //is the same as this..
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//   };
   
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+//return a string (to create HTML template)
+// const generatePage = () => 'Name: Jane, Github: janehub';
+// console.log(generatePage());
+
+fs.writeFile('index.html', generatePage(name,github), err => {
+  if (err) throw new Error(err);
+  console.log('Portfolio complete! Check out index.html to see the output!')
+});
