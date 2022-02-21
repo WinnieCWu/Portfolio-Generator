80247f23f8244ee6c1b7c96d42b481878210b0ac
WinnieCWu <winniechn7@gmail.com>
2022-02-21 12:44:37 -0800

    installed npm inquirer and check that it works

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//replace with one-liner using assignment destructuring
-const [name, github] = profileDataArgs;
-
+// const profileDataArgs = process.argv.slice(2);
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+// //replace with one-liner using assignment destructuring
+// const [name, github] = profileDataArgs;
 
 // const printProfileData = profileDataArr => {
 //     //This..
@@ -25,7 +21,22 @@ const [name, github] = profileDataArgs;
 // const generatePage = () => 'Name: Jane, Github: janehub';
 // console.log(generatePage());
 
-fs.writeFile('index.html', generatePage(name,github), err => {
-  if (err) throw new Error(err);
-  console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// ---need the bottom code occasionally.. keep const at top of page--
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw (err);
+//   console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
